0ae472c6248298f88ff4e26673fe460a2d08524a
Patrick Vézina <patrick@croustille.io>
2019-02-14 11:50:26 -0500

    Update to vagrant-sudoers.sh script

--- vagrant-sudoers.sh
+++ vagrant-sudoers.sh
@@ -9,7 +9,7 @@ cat > $TMP <<EOF
 # Allow passwordless startup of Vagrant when using NFS.
 Cmnd_Alias VAGRANT_EXPORTS_ADD = /usr/bin/tee -a /etc/exports
 Cmnd_Alias VAGRANT_NFSD = /sbin/nfsd restart
-Cmnd_Alias VAGRANT_EXPORTS_REMOVE = /usr/local/bin/sed -E -e /*/ d -ibak /etc/exports
+Cmnd_Alias VAGRANT_EXPORTS_REMOVE = /usr/bin/sed -E -e /*/ d -ibak /etc/exports
 %admin ALL=(root) NOPASSWD: VAGRANT_EXPORTS_ADD, VAGRANT_NFSD, VAGRANT_EXPORTS_REMOVE
 
 # Allow passwordless startup of Vagrant with vagrant-hostsupdater.
