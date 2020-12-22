390270282480d63b978a1c4881aae582d1f75b2f
Patrick Vézina <patrick@croustille.io>
2020-12-22 10:37:49 -0500

    hyperjs

--- .hyper.js
+++ .hyper.js
@@ -1,12 +1,10 @@
 module.exports = {
   config: {
-    verminal: {
-      fontSize: 15,
-      fontFamily:
-        'Apercu Mono, "SF Mono", "Monaco", "Inconsolata", "Fira Mono", "Droid Sans Mono", "Source Code Pro", monospace',
-      fontWeightBold: "normal",
-    },
+    fontSize: 15,
+    fontFamily:
+      'Apercu Mono, "SF Mono", "Monaco", "Inconsolata", "Fira Mono", "Droid Sans Mono", "Source Code Pro", monospace',
+    fontWeightBold: "normal",
   },
 
-  plugins: ["verminal", "hypercwd", "hyper-pane"],
+  plugins: ["hyper-solarized-dark", "hypercwd", "hyper-pane"],
 };
