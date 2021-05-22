8069c7229274e3130e9d272f10898cc5e887c5e7
Patrick Vézina <patrick@croustille.io>
2021-05-22 11:23:56 -0400

    Review hyper js

--- .hyper.js
+++ .hyper.js
@@ -1,9 +1,11 @@
 module.exports = {
   config: {
+    copyOnSelect: true,
     fontSize: 15,
     fontFamily:
       'Apercu Mono, "SF Mono", "Monaco", "Inconsolata", "Fira Mono", "Droid Sans Mono", "Source Code Pro", monospace',
     fontWeightBold: "normal",
+    scrollback: 10000,
   },
 
   plugins: ["hyper-solarized-dark", "hypercwd", "hyper-pane"],
